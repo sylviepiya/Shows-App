261f57a1c8f1e72c620b22b91fddaedd85d71a0e
Sylvie Piya <sylviepiya@hotmail.com>
2020-10-10 23:43:04 -0400

    Added song for splash screen, now final commit

--- app/src/main/java/com/example/showsapp/Splash.java
+++ app/src/main/java/com/example/showsapp/Splash.java
@@ -3,6 +3,7 @@ package com.example.showsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import java.util.Timer;
@@ -10,15 +11,21 @@ import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
+    MediaPlayer mdUkulele;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        mdUkulele=new MediaPlayer();
+        mdUkulele=MediaPlayer.create(this, R.raw.ukulele);
+        mdUkulele.start();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 finish();
+                mdUkulele.stop();
                 startActivity(new Intent(Splash.this, ItemListActivity.class));
             }
         };
